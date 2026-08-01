6f75b54f82cde15d90cf151efc1e259dd6272f65
venuscardiel-cpu <venuscardiel@gmail.com>
2026-08-02 01:56:36 +0200

    app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const clientSupabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
     try {
         // 📡 Leemos tus platos directamente desde la nube dinámica de Supabase
         const { data, error } = await clientSupabase
-            .from('recetas_aldea')
+            .from('recetas')
             .select('*');
 
         if (error) throw error;
